41045d78f8ea8660fc8b95f386f9e2a96d395f2a
savigurjar <savigurjar1201@gmail.com>
2025-04-17 04:26:11 +0530

    revision

---
+++ React/Redux-4/src/script.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+function App() {
+    
+}
+const root = ReactDOM.createRoot(document.getElementById("root")).render(
+  <App />
+);
